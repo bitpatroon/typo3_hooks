a1e281fef4d5587128d71919ffb1482a9b8747ba
Bitpatroon <github@bitpatroon.nl>
2021-04-19 10:24:53 +0200

    Added extension dependency typo3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title'                         => 'Typo3 Hooks',
-    'description'                   => 'Allows hooks throughout the system',
-    'category'                      => 'misc',
-    'author'                        => 'Sjoerd Zonneveld',
-    'author_email'                  => 'typo3@bitpatroon.nl',
-    'state'                         => 'stable',
-    'author_company'                => 'Bitpatroon',
-    'version'                       => '1.0.0'
+    'title'          => 'Typo3 Hooks',
+    'description'    => 'Allows hooks throughout the system',
+    'category'       => 'misc',
+    'author'         => 'Sjoerd Zonneveld',
+    'author_email'   => 'typo3@bitpatroon.nl',
+    'state'          => 'stable',
+    'author_company' => 'Bitpatroon',
+    'version'        => '10.4',
+    'constraints'    => [
+        'depends' => [
+            'typo3' => '10.4.0-10.9.9999',
+        ],
+    ],
 ];
